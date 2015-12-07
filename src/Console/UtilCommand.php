9d276e9d173e82f00f92d65aac8f3dfb56e27c45
Tjoosten <Tjoosten3@gmail.com>
2015-12-07 22:37:29 +0100

    update

---
+++ src/Console/UtilCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Hopp\DbUtil\Console;
+
+class UtilCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:send {user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send drip e-mails to a user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @internal param DripEmailer $drip
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    }
+}
